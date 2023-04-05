5a4c71bd9da9fa4d826638bb27812fded15c6783
Matías <matiasagbenitez@gmail.com>
2023-04-05 17:35:53 -0300

    heroes list

---
+++ src/heroes/components/HeroCard.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const CharactersByHero = ({ alter_ego, characters }) => {
+    // if (alter_ego === characters) return (<></>);
+    // return (
+    //     <p>{characters}</p>
+    // );
+    
+    alter_ego === characters ? <></> : <p>{characters}</p>
+}
+
+export const HeroCard = ({ hero }) => {
+  const { id, superhero, publisher, alter_ego, first_appearance, characters } =
+    hero;
+
+  const heroImageUrl = `/assets/heroes/${id}.jpg`;
+  return (
+    <div className="col">
+      <div className="card">
+        <div className="row no-gutters">
+          <div className="col-4">
+            <img src={heroImageUrl} className="card-img" alt={superhero} />
+          </div>
+
+          <div className="col-8">
+            <div className="card-body">
+                <h5 className="card-title">
+                    {superhero}
+                </h5>
+                <p className="card-text">
+                    {alter_ego}
+                </p>
+
+                {/* {
+                    (alter_ego !== characters) && <p className="card-text">{characters}</p>
+                } */}
+
+                <CharactersByHero alter_ego={alter_ego} characters={characters} />
+
+                <p className="card-text">
+                    <small className="text-muted">{first_appearance}</small>
+                </p>
+
+                <Link to={`/hero/${id}`} className="btn btn-primary">
+                    Más...
+                </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/heroes/pages/DCPage.jsx
+++ src/heroes/pages/DCPage.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React from "react";
+import { HeroList } from "../components";
 
 export const DCPage = () => {
   return (
-    <h1>DCPage</h1>
-  )
-}
+    <>
+      <h1>DC Comics</h1>
+      <hr />
+
+      <HeroList publisher="DC Comics" />
+    </>
+  );
+};
